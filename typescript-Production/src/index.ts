5e07608e25eee1aee326b0c65c4632f9e9240fa6
Girish <thoratgirish286@gmail.com>
2026-01-15 13:47:40 +0530

    Done with the typescript production basic

---
+++ typescript-Production/src/index.ts
@@ -0,0 +1,13 @@
+
+
+console.log("Hello TypeScript");
+let a : string =  "hello";
+console.log(a);
+function add(a : number , b : number) : number {
+    return a + b;
+}
+
+console.log(add(5 , 4));
+
+console.log(add(10 , 5));
+  
